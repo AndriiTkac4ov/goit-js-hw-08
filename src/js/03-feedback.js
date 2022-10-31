25d76b56133ee05cdf95131af7b9669baf262b62
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-10-31 17:10:41 +0200

    begin coding of 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+const refs = {
+    form: document.querySelector('form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
 
+// refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', onEmailInput);
+refs.textarea.addEventListener('input', onTextarealInput);
+
+function onEmailInput(event) {
+    const email = event.currentTarget.value;
+
+    // localStorage.setItem("feedback-form-state", email)
+};
+
+function onTextarealInput(event) {
+    const message = event.currentTarget.value;
+
+    // localStorage.setItem("feedback-form-state", message)
+};
+
+
+
+const createFeedback = function (event, onEmailInput, onTextarealInput) {
+    const feedbackFormState = {
+        email: onEmailInput(event),
+        message: onTextarealInput(event),
+    };
+
+    console.log(feedbackFormState);
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+};
+
+createFeedback();
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+//     console.log('This is SUBMIT');
+//     event.currentTarget.reset()
+// }
